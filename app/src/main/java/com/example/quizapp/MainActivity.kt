d3ed6b724189c2c0dbbf8f67f9321b1389d348d8
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-01-07 00:45:01 +0600

    creating two package one for model and one for ui also interect with the ui elements and navigate to the second activity which is QuestionActivity

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.quizapp.ui.QuestionActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonStart: Button = findViewById(R.id.nameInput)
+        val editTextName: EditText = findViewById(R.id.name)
+
+        buttonStart.setOnClickListener {
+            if(!editTextName.text.isEmpty()){
+                Intent(this@MainActivity,QuestionActivity::class.java).also {
+                    startActivity(it)
+                    finish()
+                }
+            }else{
+                Toast.makeText(this@MainActivity, "Please enter the name", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
